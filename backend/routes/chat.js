8c77d893cff6cadf5f4993e905b64712146628e5
kviswas1621 <kviswas1216@gmail.com>
2025-09-12 22:32:39 +0530

    feat: add get and delete thread route

--- backend/routes/chat.js
+++ backend/routes/chat.js
@@ -20,5 +20,49 @@ router.post('/test',async(req,res)=>{
     }
 })
 
+//get all threads
+router.get("/thread", async(req,res)=>{
+    try{
+        const threads= await Thread.find({}).sort({updatedAt: -1});
+        res.json(threads);
+    }catch(err){
+        res.status(500).json({error: "Failed to fetch threads"});
+    }
+});
+
+//threadid
+router.get("/thread/:threadId",async(req,res)=>{
+    const {threadId}=req.params;
+    try{
+       const threadid=await Thread.findOne({threadId});
+
+       if(!threadid){
+         res.status(404).json({error: "Thread not found"});
+       }
+       res.json(threadid);
+
+    }catch(err){
+        res.status(500).json({error: "Failed to fetch threadsid"});
+    }
+
+})
+
+//delete thread
+router.delete("/thread/:threadId",async(req,res)=>{
+    const {threadId}=req.params;
+    try{
+       const deletethread=await Thread.findOneAndDelete({threadId});
+
+       if(!deletethread){
+         res.status(404).json({error: "Thread not found"});
+       }
+       res.status(200).json({success:"Thread deleted successfully"});
+
+    }catch(err){
+        res.status(500).json({error: "Failed to delete thread"});
+    }
+
+})
+
 export default router;
 
