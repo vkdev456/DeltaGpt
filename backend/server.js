57735864e65cda5f1a0a3bc686560c7de190d106
kviswas1621 <kviswas1216@gmail.com>
2025-09-12 19:40:41 +0530

    chore: setup Connection with MongoDb Atlas

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import OpenAI from "openai";
 import dotenv from "dotenv";
 import cors from "cors";
 import express from "express";
+import mongoose from "mongoose";
 
 dotenv.config();
 
@@ -13,36 +14,20 @@ app.use(cors());
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
+  connectDB();
 });
 
-// app.post("/test", async (req, res) => {
-//   const options = {
-//     method: "POST",
-//     headers: {
-//       "Content-Type": "application/json",
-//       "Authorization": `Bearer ${process.env.OPENAI_API_KEY}`,
-//     },
-//     body: JSON.stringify({
-//       model: "gpt-4o-mini",
-//       messages: [
-//         {
-//           role: "user",
-//           content: req.body.message,
-//         },
-//       ],
-//     }),
-//   };
 
-//   try {
-//     const response = await fetch("https://api.openai.com/v1/chat/completions", options);
-//     const data = await response.json(); 
-//     console.log(data);
-//     res.json(data); 
-//   } catch (err) {
-//     console.error(err);
-//   }
-// });
-
+const connectDB = async()=>{
+   try{
+        await mongoose.connect(process.env.MONGODB_URL);
+        
+        console.log("connected to DB");
+   }
+   catch(err){
+    console.log("Connection Failed",err)
+   }
+}
 
 
 
