72a5680d800a02e0781dcc10e05435e38ae28564
vkdev456 <kviswas1216@gmail.com>
2025-09-17 22:35:05 +0530

    Deploy

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -10,7 +10,7 @@ function Sidebar(){
     
     const getAllThreads = async()=>{
           try{
-            const response=await fetch("http://localhost:8080/api/thread");
+            const response=await fetch("https://deltagpt.onrender.com/api/thread");
             const res=await response.json();
             const filteredData =res.map(thread=>({threadId: thread.threadId, title: thread.title}));
             setAllThreads(filteredData);
@@ -36,7 +36,7 @@ function Sidebar(){
     const changeThread= async (newThreadId)=>{
        setCurrThreadId(newThreadId);
        try{
-           const response= await fetch(`http://localhost:8080/api/thread/${newThreadId}`);
+           const response= await fetch(`https://deltagpt.onrender.com/api/thread/${newThreadId}`);
            const res=await response.json();
            console.log(res.messages);
            setPrevChats(res.messages || []);  
@@ -50,7 +50,7 @@ function Sidebar(){
     
     const deleteThread = async(threadId) => {
         try{
-            const response=await fetch(`http://localhost:8080/api/thread/${threadId}`,{method: "DELETE"});
+            const response=await fetch(`https://deltagpt.onrender.com/api/thread/${threadId}`,{method: "DELETE"});
             const res=await response.json();
             console.log(res);
 
